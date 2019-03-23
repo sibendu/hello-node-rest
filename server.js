3928482422cbc16a7c6fa53af7282f2bfe0d5c9e
sibendu <sibendu@users.noreply.github.com>
2019-03-23 10:38:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/users', function (req, res) {
    });
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(5000, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
