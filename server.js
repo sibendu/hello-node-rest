3f9f8a4cd31bc39c243bb30b89b00d35f68cb509
sibendu <sibendu@users.noreply.github.com>
2019-03-23 10:24:46 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/users', function (req, res) {
+   fs.readFile( "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+app.get('/users/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile("users.json", 'utf8', function (err, data) {
+      var users = JSON.parse( data );
+      var user = users["user" + req.params.id] 
+      console.log( user );
+      res.end( JSON.stringify(user));
+   });
+})
+
+app.post('/users', function (req, res) {
+   // First read existing users.
+   fs.readFile("users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      data["user4"] = user["user4"];
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
